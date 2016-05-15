122b44df7fadb1c3f0fe867a0243919083113258
Rubén D <rdelgado25007@gmail.com>
2016-05-15 23:45:25 +0200

    Segundo JUnit

---
+++ src/test/TestUsuario.java
@@ -0,0 +1,48 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUsuario {
+
+	@Test
+	public void testUsuario() {
+	}
+
+	@Test
+	public void testGetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetApellidos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalcularPuntuacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDarPuntuacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPuntos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testImprimir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+}
